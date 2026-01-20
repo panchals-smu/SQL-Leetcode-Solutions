88d421b3d4fc1c48b120f578ef14cd5a51c2a69e
Shraddha Panchal <panchals@smu.edu>
2026-01-20 12:21:51 -0600

    Time: 444 ms (9.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+-- SELECT DISTINCT p1.email as Email
+-- FROM Person p1
+-- JOIN Person p2
+-- ON p1.email = p2.email
+-- WHERE p1.id != p2.id
+
+select email from person
+group by email
+having count(email) > 1
